a84f16fc318ebff62b3e0ed60ce18473e51d9723
Franko Leon Tokalić <franko.tokalic@arsfutura.co>
2022-04-19 18:31:35 +0200

    Remove the `MainActivity` lifecycle callbacks and the click listener.

--- app/src/main/java/co/arsfutura/fronted/eventhandlingdemo/MainActivity.java
+++ app/src/main/java/co/arsfutura/fronted/eventhandlingdemo/MainActivity.java
@@ -15,53 +15,6 @@ public class MainActivity extends AppCompatActivity {
 
     private Button buttonDraw;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        Log.d(TAG, "onCreate() called with: savedInstanceState = [" + savedInstanceState + "]");
-        setContentView(R.layout.activity_main);
-        buttonDraw = findViewById(R.id.button_draw);
-        buttonDraw.setOnClickListener(v -> navigateToDrawingActivity());
-    }
-
-    @Override
-    protected void onRestart() {
-        super.onRestart();
-        Log.d(TAG, "onRestart() called");
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Log.d(TAG, "onStart() called");
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Log.d(TAG, "onResume() called");
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Log.d(TAG, "onPause() called");
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Log.d(TAG, "onStop() called");
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Log.d(TAG, "onDestroy() called");
-        buttonDraw.setOnClickListener(null);
-        buttonDraw = null;
-    }
-
     private void navigateToDrawingActivity() {
         startActivity(DrawingActivity.newIntent(this));
     }
